b09786be73922a537fc311c2f04d26eafad0232a
Harsh <harshagarwal91016@gmail.com>
2026-01-03 11:15:29 +0530

    first commit

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -6,11 +6,11 @@ const server = new WebSocketServer({
 });
 
 server.on("connection", async (socket) => {
-    client.user.create({
+    const user=await client.user.create({
         data: {
             username: Math.random().toString(),
             password: Math.random().toString()
         }
     })
-    socket.send("Hi there you are connected to the server");
+    socket.send(`Hi there you are connected to the server ${user.id}`);
 })
